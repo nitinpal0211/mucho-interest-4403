d740dc73e0a45e3184754cab455c7146ab8a003e
Chetan Patil <cp02589@gmail.com>
2022-12-22 13:06:55 +0530

    Exception Handling completion
    NoHandlerFound, methodArgumentNotValid,
    illegalArgumentException
    Exceptions created.

--- Plant_Nursery_Application/src/main/java/com/masai/Exceptions/GlobalExceptionHandler.java
+++ Plant_Nursery_Application/src/main/java/com/masai/Exceptions/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -44,6 +46,35 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<MyErrorDetails>(err,HttpStatus.BAD_REQUEST);
 	}
 	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException me){
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimestap(LocalDateTime.now());
+		err.setMessage("Validation Error");
+		err.setDescription(me.getBindingResult().getFieldError().getDefaultMessage());
+		
+		return new ResponseEntity<MyErrorDetails>(err,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<MyErrorDetails> illegalArgumnetExceptionHandler(IllegalArgumentException ie, WebRequest req ){
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimestap(LocalDateTime.now());
+		err.setMessage(ie.getMessage());
+		err.setDescription(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<MyErrorDetails> noHandlerFoundExceptionHandler(NoHandlerFoundException ne, WebRequest req ){
+		MyErrorDetails err = new MyErrorDetails();
+		err.setTimestap(LocalDateTime.now());
+		err.setMessage(ne.getMessage());
+		err.setDescription(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err,HttpStatus.BAD_REQUEST);
+	}
 	
 	public ResponseEntity<MyErrorDetails> allExceptionHandler(Exception e,WebRequest req){
 		MyErrorDetails err = new MyErrorDetails();
